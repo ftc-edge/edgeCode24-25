20fb8c31daefc8eb44a0dfd03688574f05b2026d
GamingWithAPengu <148139364+GamingWithAPengu@users.noreply.github.com>
2024-10-09 12:27:36 -0400

    added a function to check if someone is pressing the right stick - debug functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/standardDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/standardDrive.java
@@ -2,32 +2,53 @@ package org.firstinspires.ftc.teamcode.opModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.ProgrammingBoard;
 @TeleOp()
 public class standardDrive extends OpMode{
-    private ProgrammingBoard board;
+    public DcMotor FRmotor;
+    public DcMotor FLmotor;
+    public DcMotor BRmotor;
+    public DcMotor BLmotor;
     @Override
     public void init(){
+        FRmotor = hardwareMap.get(DcMotor.class, "FRmotor");
+        FRmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        FLmotor = hardwareMap.get(DcMotor.class, "FLmotor");
+        FLmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        BRmotor = hardwareMap.get(DcMotor.class, "BRmotor");
+        BRmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        BLmotor = hardwareMap.get(DcMotor.class, "BLmotor");
+        BLmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     public void loop(){
         doMove();
+
+        if(gamepad1.right_stick_x > 0f){
+            System.out.print("AHHHHHHH");
+        }
     }
 
     public void doMove(){
-        double forward = gamepad1.left_stick_y;
-        double horizontal = gamepad1.left_stick_x;
-        double pivot = gamepad1.left_stick_x;
+        float forward;
+        float horizontal;
+        float pivot;
 
-        board.FLmotor.setPower(pivot + (-forward + horizontal));
-        board.FRmotor.setPower(pivot + (-forward - horizontal));
-        board.BRmotor.setPower(pivot + (-forward - horizontal));
-        board.BLmotor.setPower(pivot + (-forward + horizontal));
+        forward = -gamepad1.left_stick_y;
+        horizontal = gamepad1.left_stick_x;
+        pivot = gamepad1.right_stick_x;
+
+        FRmotor.setPower(pivot + (-forward + horizontal));
+        BRmotor.setPower(pivot + (-forward - horizontal));
+        FLmotor.setPower(pivot + (-forward - horizontal));
+        BLmotor.setPower(pivot + (-forward + horizontal));
     }
 
-    public void doSlides(){
+    //public void doSlides(){
         
-    }
+    //}
 }
